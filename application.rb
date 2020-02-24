30104b604fd62031b89ebee9e60141a1bfb74456
github username <meliajm@gmail.com>
2020-02-23 16:51:40 -0800

    Done.

--- application.rb
+++ application.rb
@@ -1,10 +1,20 @@
 class Application
 
+  # def call(env)
+  #   resp = Rack::Response.new
+  #   resp.write "Hello, my name is"
+  #   resp.finish
+  # end
+
   def call(env)
-    resp = Rack::Response.new
-    resp.write "Hello, World"
-    resp.finish
+    return [ 200, {'Content-Type' => 'text/html'}, pretty_response ]
+  end
+  
+  def pretty_response
+    (Time.now.to_i % 2).zero? ?  ["<em>Hello</em>"] : ["<strong>Hello</strong>"]
   end
 
 end
+
+
 
